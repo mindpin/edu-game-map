083e9c4d0986484e1c4772cd930d32da4f84e50e
Kaid <kaid@kaid.me>
2014-11-20 16:10:34 +0800

    rewrite example seed

--- example/lib/seed.rb
+++ example/lib/seed.rb
@@ -8,17 +8,25 @@ def mklesson(course, x)
   Lesson.find_or_create_by(:name => "#{course.name} - 第#{x}节", :course_id => course.id)
 end
 
+#
+# course1和course2 分别对应
+# https://github.com/mindpin/edu-game-map/issues/2#issuecomment-63586844
+# 中两张图里的 m1, m2
+#
 puts "======== 开始生成数据"
-1.upto(8).each do |x|
+1.upto(2).each do |x|
   course = mkcourse(x)
 
-  count = rand 6..23
+  count = case x
+          when 1 then 11
+          when 2 then 4
+          end
 
-  1.upto(6 + (7..count).size).each do |y|
+  1.upto(count).each do |y|
     mklesson(course, y)
 
     nr = y == count ? "\n" : "\r"
-    print "-- 课程(#{x}/8) - 课节(#{y}/#{count})#{nr}"
+    print "-- 课程(#{x}/2) - 课节(#{y}/#{count})#{nr}"
   end
 end
 puts "======== 结束生成数据"
