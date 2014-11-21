03c70d8f15e4bd31650761c49b60015bc856cb91
fushang318 <fushang318@gmail.com>
2014-11-21 15:15:52 +0800

    fix

--- lib/edu-game-map/map/json_methods.rb
+++ lib/edu-game-map/map/json_methods.rb
@@ -8,14 +8,14 @@ module EduGameMap
       end
 
       def _set_map_id_and_name
-        json_map_id   = _json_hash["id"]
-        json_map_name = _json_hash["name"]
+        json_map_id   = json_hash["id"]
+        json_map_name = json_hash["name"]
 
         self.map_id   = json_map_id   if json_map_id   != self.map_id
         self.map_name = json_map_name if json_map_name != self.map_name
       end
 
-      def _json_hash
+      def json_hash
         @json_hash ||= JSON.parse(self.json)
       end
 
@@ -32,16 +32,16 @@ module EduGameMap
       def _parse_json_hash
         @begin_nodes || begin
           id__node_hash = {}
-          _json_hash["nodes"].each do |node_hash|
+          json_hash["nodes"].each do |node_hash|
             id__node_hash[node_hash["id"]] = EduGameMap::Node.new(self, node_hash)
           end
-          _json_hash["relations"].each do |relation|
+          json_hash["relations"].each do |relation|
             parent = id__node_hash[relation["parent"]]
             child = id__node_hash[relation["child"]]
             parent.send(:_add_child, child)
             child.send(:_add_parent, parent)
           end
-          _json_hash["jump"].each do |jump|
+          json_hash["jump"].each do |jump|
             node = id__node_hash[jump["node"]]
             map = EduGameMap::Map.where(:map_id => jump["map"]).first
             node.send(:_set_jump_to_map, map)
--- lib/edu-game-map/map_learned_record/json_methods.rb
+++ lib/edu-game-map/map_learned_record/json_methods.rb
@@ -8,18 +8,18 @@ module EduGameMap
       end
 
       def _set_map_id
-        json_map_id  = _json_hash["map"]
-        json_user_id = _json_hash["user"]
+        json_map_id  = json_hash["map"]
+        json_user_id = json_hash["user"]
         self.map_id  = json_map_id  if json_map_id  != self.map_id
         self.user_id = json_user_id if json_user_id != self.user_id
       end
 
-      def _json_hash
+      def json_hash
         @json_hash ||= JSON.parse(self.json)
       end
 
       def learned_node_ids
-        _json_hash["learned_nodes"]
+        json_hash["learned_nodes"]
       end
 
     end
