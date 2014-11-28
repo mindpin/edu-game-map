bc4ed92722a18e918b332ad17113c78974c0f03f
fushang318 <fushang318@gmail.com>
2014-11-28 16:36:09 +0800

    fix

--- lib/edu-game-map/map/json_methods.rb
+++ lib/edu-game-map/map/json_methods.rb
@@ -38,16 +38,16 @@ module EduGameMap
         return if !__parse_json_hash_cache_expired? && !@begin_nodes.nil?
 
         id__node_hash = {}
-        json_hash["nodes"].each do |node_hash|
+        (json_hash["nodes"]||[]).each do |node_hash|
           id__node_hash[node_hash["id"]] = EduGameMap::Node.new(self, node_hash)
         end
-        json_hash["relations"].each do |relation|
+        (json_hash["relations"]||[]).each do |relation|
           parent = id__node_hash[relation["parent"]]
           child = id__node_hash[relation["child"]]
           parent.send(:_add_child, child)
           child.send(:_add_parent, parent)
         end
-        json_hash["jump"].each do |jump|
+        (json_hash["jump"]||[]).each do |jump|
           node = id__node_hash[jump["node"]]
           map = EduGameMap::Map.where(:map_id => jump["map"]).first
           node.send(:_set_jump_to_map, map)
